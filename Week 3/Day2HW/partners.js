79ca675540616252442b30d31cefcfd1fe680f27
VonBorstel-J <jordanvonborstel@gmail.com>
2022-09-13 14:48:25 -0700

    90% done with day2 hw

---
+++ Week 3/Day2HW/partners.js
@@ -0,0 +1,17 @@
+var photos = []; 
+var fileNames = []; 
+var imageList = []; 
+var image = []; 
+var openList = "<li class='partner'>"; 
+var closeList = "</li>";
+
+
+for (var i=0; i<6; i++) { 
+    fileNames.push("partner" + (i+1)); 
+    photos.push("<img src='/partners/"+fileNames[i]+".png'>");
+    image = openList + photos[i] + closeList; 
+    imageList.push(image); 
+}
+
+document.getElementById("partners").innerHtml = imageList; 
+
