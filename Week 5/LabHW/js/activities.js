bad608d0d5fa8da94895cca996050dafff0e39fd
VonBorstel-J <jordanvonborstel@gmail.com>
2022-10-04 20:37:24 -0700

    groupchallenge done

--- Week 5/LabHW/js/activities.js
+++ Week 5/LabHW/js/activities.js
@@ -7,26 +7,24 @@ $(document).ready(function () {
     if (content != "Not Available") {
       $(this).toggleClass("clicked");
 
-    //   var cliff = $("th").text();
+      //   var cliff = $("th").text();
       var head1 = $("th").eq(1).text();
       var head2 = $("th").eq(2).text();
       var head3 = $("th").eq(3).text();
       var head4 = $("th").eq(4).text();
 
+      if ($(this).hasClass("clicked")) {
+        $("#displaySelected").css("visibility", "visible");
+        $("#displaySelected").css("margin-top", "2em");
+        $("#result").append("<p>" + content + ":   " + "</p>");
+      } else {
+        $("#result p:contains(" + content + ")").remove();
 
-        if ($(this).hasClass("clicked")) {
-          $("#displaySelected").css("visibility", "visible");
-          $("#displaySelected").css("margin-top", "2em");
-          $("#result").append("<p>" + content +':   '+"</p>");
-        } else {
-          $("#result p:contains(" + content + ")").remove();
-
-          if ($("#result").has("p").length == false) {
-            $("#displaySelected").css("visibility", "hidden");
-            $("#displaySelected").css("margin-top", "0");
-          }
-
+        if ($("#result").has("p").length == false) {
+          $("#displaySelected").css("visibility", "hidden");
+          $("#displaySelected").css("margin-top", "0");
         }
       }
-    });
+    }
   });
+});
