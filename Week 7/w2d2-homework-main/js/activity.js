5751a71ca0a6ac9162cd374be6d9f334aa0a5cf8
VonBorstel-J <jordanvonborstel@gmail.com>
2022-10-14 12:15:56 -0700

    removed old files

--- Week 7/w2d2-homework-main/js/activity.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-$('td').css('cursor', 'pointer');
-$("td").click(function() {
-    var content = $(this).text(); 
-    if (content != "Not Available"){ 
-        $(this).toggleClass("tdhighlight");
-        }
-    });
-});
