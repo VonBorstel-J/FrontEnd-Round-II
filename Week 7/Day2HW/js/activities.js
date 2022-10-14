ad0eb3c432aa8694d6931c1b368ab43f5dcda54a
VonBorstel-J <jordanvonborstel@gmail.com>
2022-10-14 12:15:06 -0700

    Day2 Hw done

---
+++ Week 7/Day2HW/js/activities.js
@@ -0,0 +1,45 @@
+//--------jQuery Table Scripts-----------------------------------------------------------------------------------------------------------------------------------
+
+// $(document).ready(function () {
+//   $("td").css("cursor", "pointer");
+//   $("td").click(function () {
+//     var content = $(this).text();
+//     if (content != "Not Available") {
+//       $(this).toggleClass("clicked");
+
+//       //   var cliff = $("th").text();
+//       var head1 = $("th").eq(1).text();
+//       var head2 = $("th").eq(2).text();
+//       var head3 = $("th").eq(3).text();
+//       var head4 = $("th").eq(4).text();
+
+//       if ($(this).hasClass("clicked")) {
+//         $("#activityModal").css("visibility", "visible");
+//         $("#activityText").append("<p>" + content + ":   " + "</p>");
+//       } else {
+//         $("#result p:contains(" + content + ")").remove();
+
+//         if ($("#result").has("p").length == false) {
+//           $("#displaySelected").css("visibility", "hidden");
+//           $("#displaySelected").css("margin-top", "0");
+//         }
+//       }
+//     }
+//   });
+// });
+
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Stuck........ Losttttt........ 
+//     $(document).ready(function() {
+//         var trText = 'doot doot'
+//         $('#activityModal').modal( {
+//             keyboard: true,
+//             backdrop: "static",
+//             show:false,
+//             $('#activityText').bind('click', function(){ 
+//                 $(this).toggleClass('blue');
+//             });
+//     });
+// });
